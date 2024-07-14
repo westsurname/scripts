e4123eabb3a28c5098d56e7d7c7de051b13dd5c2
Pukabyte <120460627+Pukabyte@users.noreply.github.com>
2024-07-15 02:02:46 +1200

    Update discord.py

--- shared/discord.py
+++ shared/discord.py
@@ -3,13 +3,12 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 from shared.shared import discord, checkRequiredEnvs
 
 def validateDiscordWebhookUrl():
-        url = discord['webhookUrl']
-        try:
-            response = requests.get(url)
-            return response.status_code == 200
-        except Exception as e:
-            return False
-
+    url = discord['webhookUrl']
+    try:
+        response = requests.get(url)
+        return response.status_code == 200
+    except Exception as e:
+        return False
 
 requiredEnvs = {
     'Discord webhook URL': (discord['webhookUrl'], validateDiscordWebhookUrl)
@@ -28,14 +27,20 @@ def discordError(title, message=None):
             embeds=[embed]
         )
         response = webhook.execute()
+        return response.json().get('id')
 
-def discordUpdate(title, message=None):
-    if discord['updateEnabled']:
-        embed = DiscordEmbed(title, message, color=3066993)
-        webhook = DiscordWebhook(
-            url=discord['webhookUrl'], 
-            rate_limit_retry=True, 
-            username='Update Bot', 
-            embeds=[embed]
-        )
+def discordUpdate(title, message=None, color=3066993, message_id=None):
+    embed = DiscordEmbed(title=title, description=message, color=color)
+    webhook = DiscordWebhook(
+        url=discord['webhookUrl'], 
+        rate_limit_retry=True, 
+        username='Update Bot'
+    )
+    webhook.add_embed(embed)
+
+    if message_id:
+        webhook.id = message_id
+        response = webhook.edit()
+    else:
         response = webhook.execute()
+        return response.json().get('id')
