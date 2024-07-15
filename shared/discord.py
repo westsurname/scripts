a15f6e1d2c172d7c3c577096ad1a8a173c94f39f
Pukabyte <120460627+Pukabyte@users.noreply.github.com>
2024-07-16 10:38:20 +1200

    Update discord.py

--- shared/discord.py
+++ shared/discord.py
@@ -1,6 +1,7 @@
 import requests
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from shared.shared import discord, checkRequiredEnvs
+from datetime import datetime
 
 def validateDiscordWebhookUrl():
     url = discord['webhookUrl']
@@ -29,12 +30,18 @@ def discordError(title, message=None):
         response = webhook.execute()
         return response.json().get('id')
 
-def discordUpdate(title, message=None, color=3066993, message_id=None):
+def discordUpdate(title, message=None, color=3066993, message_id=None, content=None, timestamp=None):
     embed = DiscordEmbed(title=title, description=message, color=color)
+    embed.set_author(name="Blackhole", icon_url="https://cdn.discordapp.com/attachments/1222893572521594962/1262384669161295934/black-hole-556660.png?ex=669666d7&is=66951557&hm=e792b1162ea86c1ba24ee9f4568c571ff1e3429e5b295555fea3fcb94404b434&")
+    
+    if timestamp:
+        embed.set_timestamp(datetime.fromisoformat(timestamp[:-1]))
+
     webhook = DiscordWebhook(
         url=discord['webhookUrl'], 
         rate_limit_retry=True, 
-        username='Update Bot'
+        username='Update Bot',
+        content=content
     )
     webhook.add_embed(embed)
 
